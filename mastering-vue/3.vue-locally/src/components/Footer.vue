6d8514b3055c8d8f134f0f4ef23d6716cbb20547
EricWanjohiMuturi <emuturi124@gmail.com>
2025-12-08 22:56:59 +0300

    Single File Components

---
+++ mastering-vue/3.vue-locally/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import {ref} from 'vue'
+    const year = ref('2025')
+</script>
+
+<template>
+    <footer>
+        <p>&copy; <span class="year">{{ year }}</span> </p>
+    </footer>
+</template>
+
+<style scoped>
+  footer {
+      position:fixed;
+      bottom:0;
+      padding-bottom:20px;
+  }
+  .year{
+    color: blue;
+  }
+</style>
